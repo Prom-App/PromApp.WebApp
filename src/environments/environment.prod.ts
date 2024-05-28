a1094c4494db663813fe78d483351d2a3d4ccfc4
tecnologiaprom <tecnologia@promapp.ai>
2024-05-28 11:40:30 -0500

    adding enviroment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  environment: "local"
+  environment: "local",
+  api:'https://promappadminapi.azurewebsites.net/api/v1/',
+  clientId: '1032760841710-g8n6qlnda728pj3le812kv545l33rdfr.apps.googleusercontent.com',
+  filenameCsv: "dev-pos"
 };
